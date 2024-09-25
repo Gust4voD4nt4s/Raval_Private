0971788cb474ae91a0d8b92b17a946cffe852e70
Gust4vo D4nt4s <gustavo1234henrique.gd@gmail.com>
2024-09-24 23:24:42 -0300

    cors atualizado

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,4 @@
 export default {
-    origin: 'http://localhost:3000',
+    origin: 'https://front--ravalimobiliaria.netlify.app',
     baseUrl: 'postgresql://dbraval_user:019csgwlOcehrriCS2Q8uVZi5oQNA9Ie@dpg-crpla1l6l47c73al3pg0-a.oregon-postgres.render.com/dbraval'
 };  
